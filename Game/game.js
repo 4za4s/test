469fee3e112f8d6e000d518d366ece4784ac95a5
4za4s <arbo3802@gmail.com>
2019-06-08 11:25:42 +1000

    fixed save for the final time or ill quit

--- Game/game.js
+++ Game/game.js
@@ -1,13 +1,13 @@
 var machineArray = [];
 //amount, cost, output, where, costScale, name, click increase amount
 machineArray.push([0, 0, 0, 0, 1, "0", 0.01]); //points
-machineArray.push([0, 1, 0.00001, 0, 1.01, "1", 0.1]); //1
-machineArray.push([0, 10, 0.0001, 1, 1.005, "2", 1]); //2
-machineArray.push([0, 100, 0.001, 2, 1.001, "3", 1]); //3
-machineArray.push([0, 1000, 0.01, 3, 1.001, "4", 1]); //4
+machineArray.push([0, 1, 0.0001, 0, 1.01, "1", 0.1]); //1
+machineArray.push([0, 10, 0.001, 1, 1.005, "2", 1]); //2
+machineArray.push([0, 100, 0.01, 2, 1.001, "3", 1]); //3
+machineArray.push([0, 1000, 0.1, 3, 1.001, "4", 1]); //4
 machineArray.push([0, 10000, 0.1, 4, 1.001, "5", 1]); //5
 machineArray.push([0, 100000, 1, 5, 1.001, "6", 1]); //6
-machineArray.push([0, 1000000, 10, 6, 1.001, "7", 1]); //7
+machineArray.push([0, 1000000, 1, 6, 1.001, "7", 1]); //7
 
 function press(machine){
 	if(machineArray[0][0] >= machineArray[machine][1]){
@@ -38,27 +38,29 @@ function saver(){
 }
 
 function load(){
-	if(typeof save !== "undefined"){
-		var savegame = JSON.parse(localStorage.getItem("save"));
-		if (typeof savegame !== "null"){
-			machineArray = savegame;
+	var savegame = JSON.parse(localStorage.getItem("save"));
+	if (savegame !== null){
+		machineArray = savegame;
+		for(var i = 1; i < machineArray.length; i++){
+			document.getElementById(machineArray[i][5]).innerHTML = machineArray[i][0];
+			var cost = "cost" + machineArray[i][5];
+			document.getElementById(cost).innerHTML = machineArray[i][1];
 		}
 	}
 }
-
 
 function removeSave(){
 	localStorage.removeItem("save");
 	window.location.reload();
 }
 
-/*window.setInterval(function(){
+window.setInterval(function(){
 	loops();
 }, 200);
-*/
+
 window.setInterval(function(){
 	saver();
-}, 60000);
+}, 50000);
 
 load();
 
